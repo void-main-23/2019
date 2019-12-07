9aafbc1b12657b5bed6ff168751871238eb91314
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-12-07 20:40:51 +0530

    Add another for loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ public class Main {
         for (int i=0; i<10; i++){
             System.out.println(i);
         }
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+        }
     }
 }
