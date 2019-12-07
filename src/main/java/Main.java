3c930b2496af118e64033c26c8849bb701390220
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-12-07 20:36:04 +0530

    Add while loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,9 @@ public class Main {
         for (int i=0; i<10; i++){
             System.out.println(i);
         }
+        int a = 10;
+        while(a<10){
+            System.out.println("1");
+        }
     }
 }
